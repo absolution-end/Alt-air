0b976711d945e0a766e985aa7acf3a2e4ba1d2da
chirayu Sharma <111888038+absolution-end@users.noreply.github.com>
2023-10-29 07:02:29 +0530

    Add files via upload

---
+++ model/architecture/deeplab_xception.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+from third_party.deeplabv3plus import Deeplabv3
+
+
+def get_network(input_shape, n_output_channels):
+
+    deeplab = Deeplabv3(input_shape=input_shape,
+                            classes=n_output_channels,
+                            backbone="xception",
+                            weights=None,
+                            activation="softmax")
+
+    i = tf.keras.layers.Input(input_shape)
+    o = deeplab(i)
+
+    return tf.keras.models.Model(i, o)
